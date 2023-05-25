266abd783bc8aaab11789244bf97ff3767286203
Miguel Ramirez <121323094+mramirez092@users.noreply.github.com>
2023-05-25 02:32:02 +0000

    Volvi a añadir usuario en el backend, me estaba dando problemas. Esta hecho el endpoint para obtener y hacer post de los animales

--- src/api/models.py
+++ src/api/models.py
@@ -2,6 +2,22 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(80), unique=False, nullable=False)
+    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+            # do not serialize the password, its a security breach
+        }
+
 class Animal(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(50), unique=False, nullable=False)
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,7 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, Animal
 from api.utils import generate_sitemap, APIException
 
 api = Blueprint('api', __name__)
@@ -15,4 +15,29 @@ def handle_hello():
         "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
     }
 
+    return jsonify(response_body), 200
+
+#ANIMAL ENDPOINT -------------------------------------------------------------------
+#GET
+@api.route('/animal', methods=['GET'])
+def get_animals():
+    allAnimals = Animal.query.all()
+    result = [element.serialize() for element in allAnimals]
+    return jsonify(result), 200
+
+#POST
+@api.route('/animal', methods=['POST'])
+def post_animal():
+
+    # obtener los datos de la petición que están en formato JSON a un tipo de datos entendibles por pyton (a un diccionario). En principio, en esta petición, deberían enviarnos 3 campos: el nombre, la descripción del planeta y la población
+    data = request.get_json()
+
+    # creamos un nuevo objeto de tipo Planet
+    animal = Animal(nombre=data['nombre'], raza=data['raza'], edad=data['edad'], genero=data['genero'], descripcion=data['descripcion'])
+
+    # añadimos el planeta a la base de datos
+    db.session.add(animal)
+    db.session.commit()
+
+    response_body = {"msg": "El animal fué añadido exitosamente"}
     return jsonify(response_body), 200
