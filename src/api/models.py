ddd001534b7b9cc61cb53e1527e788e3ffb7b592
Miguel Ramirez <121323094+mramirez092@users.noreply.github.com>
2023-05-25 02:09:35 +0000

    Modelo animales

--- src/api/models.py
+++ src/api/models.py
@@ -2,18 +2,23 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class Animal(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    nombre = db.Column(db.String(50), unique=False, nullable=False)
+    raza = db.Column(db.String(80), unique=False, nullable=False)
+    edad = db.Column(db.String(50), unique=False, nullable=False)
+    genero = db.Column(db.String(50), unique=False, nullable=False)
+    descripcion = db.Column(db.String(500), unique=False, nullable=False)
 
     def __repr__(self):
-        return f'<User {self.email}>'
+        return f'<Animal {self.nombre}>'
 
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
+            "nombre": self.nombre,
+            "raza": self.raza,
+            "genero": self.genero,
+            "descripcion": self.descripcion
             # do not serialize the password, its a security breach
         }
