9f333ead489f50e3143057ed33376f5cbb7837e3
Jonathan Gutiérrez <120731494+Goodtierrez@users.noreply.github.com>
2023-05-26 08:34:11 +0000

    tabla ADOPTION Y endpoints

--- src/api/models.py
+++ src/api/models.py
@@ -1,4 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import relationship
+import datetime
 
 db = SQLAlchemy()
 
@@ -25,3 +27,18 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class Adoption(db.Model):
+        id = db.Column(db.Integer, primary_key=True)
+        user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+        # animal_id = db.Column(db.Integer, db.ForeignKey('animal.id'), nullable=False)
+        date = db.Column(db.Date, default=datetime.datetime.now(), nullable=False)
+        status = db.Column(db.String(80), unique=False, nullable=False)
+
+
+        user = relationship('User', backref='adoptions')
+        # animal = relationship('Animal', backref='adoptions')
+
+        def __repr__(self):
+            return f'{self.user.name} - {self.date}'
+        # En cuanto Miguel meta la tabla Animal, añadir :   - {self.animal.name} y descomentar relationship línea 39 y 33
