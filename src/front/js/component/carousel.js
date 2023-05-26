af9c9935b7d886963187b8fac5c4e36c28022eb0
Juan Silva <123385530+juansj23@users.noreply.github.com>
2023-05-26 19:18:42 +0000

    faltan cositas

--- src/front/js/component/carousel.js
+++ src/front/js/component/carousel.js
@@ -4,21 +4,21 @@ import "../../styles/component.css"
 
 export const Carousel = () => {
     return (
-        <div className="container" id="carousel">
+        
         <div
             id="carouselExampleInterval"
             className="carousel slide"
             data-bs-ride="carousel"
         >
-            <div className="carousel-inner">
+            <div className="carousel-inner rounded">
                 <div className="carousel-item active" data-bs-interval={10000}>
-                    <img src="https://r4.wallpaperflare.com/wallpaper/644/569/83/animales-baby-cachorros-labrados-wallpaper-a8c6fc7a6f0c37194e3029e262f98bf0.jpg" className="d-block w-100" alt="Perros" />
+                    <img src="https://r4.wallpaperflare.com/wallpaper/644/569/83/animales-baby-cachorros-labrados-wallpaper-a8c6fc7a6f0c37194e3029e262f98bf0.jpg" className="d-block w-100" alt="Perros" id="imgcarousel"/>
                 </div>
                 <div className="carousel-item" data-bs-interval={2000}>
-                    <img src="https://r4.wallpaperflare.com/wallpaper/162/949/183/animales-baby-cachorros-gato-wallpaper-ef21707cbb8f81af3206eec8fa136327.jpg" className="d-block w-100" alt="Perro y Gato" />
+                    <img src="https://r4.wallpaperflare.com/wallpaper/162/949/183/animales-baby-cachorros-gato-wallpaper-ef21707cbb8f81af3206eec8fa136327.jpg" className="d-block w-100" alt="Perro y Gato" id="imgcarousel"/>
                 </div>
                 <div className="carousel-item">
-                    <img src="https://r4.wallpaperflare.com/wallpaper/202/312/967/acostado-animales-gato-hierba-wallpaper-22113260fdf6be8bcac812d5803889f2.jpg" className="d-block w-100" alt="Gato" />
+                    <img src="https://r4.wallpaperflare.com/wallpaper/202/312/967/acostado-animales-gato-hierba-wallpaper-22113260fdf6be8bcac812d5803889f2.jpg" className="d-block w-100" alt="Gato" id="imgcarousel" />
                 </div>
             </div>
             <button
@@ -40,6 +40,6 @@ export const Carousel = () => {
                 <span className="visually-hidden">Next</span>
             </button>
         </div>
-        </div>
+       
     )
 }
