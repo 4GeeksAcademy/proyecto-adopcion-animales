46b1ebc0351bb98ea165ea8751af17e19593ddf2
Juan Silva <123385530+juansj23@users.noreply.github.com>
2023-05-26 18:34:58 +0000

    faltan cosas

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -2,17 +2,14 @@ import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 
 import "../../styles/home.css";
+import { Carousel } from "../component/carousel";
 
 export const Home = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
 		<div className="text-center mt-5">
-
-			{/* <div className="alert alert-info">
-				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
-			</div>
-			 */}
+			<Carousel></Carousel>
 		</div>
 	);
 };
