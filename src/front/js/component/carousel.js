e8d50afc0efebc318d68ddf4eea42cb71b493909
Juan Silva <123385530+juansj23@users.noreply.github.com>
2023-05-29 10:28:49 +0000

    modo oscuro listo

--- src/front/js/component/carousel.js
+++ src/front/js/component/carousel.js
@@ -12,13 +12,13 @@ export const Carousel = () => {
         >
             <div className="carousel-inner rounded">
                 <div className="carousel-item active" data-bs-interval={10000}>
-                    <img src="https://r4.wallpaperflare.com/wallpaper/644/569/83/animales-baby-cachorros-labrados-wallpaper-a8c6fc7a6f0c37194e3029e262f98bf0.jpg" className="d-block w-100" alt="Perros" id="imgcarousel"/>
+                    <img src="https://via.placeholder.com/600x400" className="d-block w-100" alt="Perros" id="imgcarousel"/>
                 </div>
                 <div className="carousel-item" data-bs-interval={2000}>
-                    <img src="https://r4.wallpaperflare.com/wallpaper/162/949/183/animales-baby-cachorros-gato-wallpaper-ef21707cbb8f81af3206eec8fa136327.jpg" className="d-block w-100" alt="Perro y Gato" id="imgcarousel"/>
+                    <img src="https://via.placeholder.com/600x400" className="d-block w-100" alt="Perro y Gato" id="imgcarousel"/>
                 </div>
                 <div className="carousel-item">
-                    <img src="https://r4.wallpaperflare.com/wallpaper/202/312/967/acostado-animales-gato-hierba-wallpaper-22113260fdf6be8bcac812d5803889f2.jpg" className="d-block w-100" alt="Gato" id="imgcarousel" />
+                    <img src="https://via.placeholder.com/600x400" className="d-block w-100" alt="Gato" id="imgcarousel" />
                 </div>
             </div>
             <button
--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,47 +1,66 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
-import"../../styles/component.css"
+import { Context } from "../store/appContext";
+import "../../styles/component.css";
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-expand-lg navbar-light" id="navbar">
-			<div className="container-fluid" >
-				<a className="navbar-brand" href="/" >
-					
-						<span className="navbar-brand mb-0 h1">No Tenemos Nombre Muchachos</span>
-					
-				</a>
-				<button
-					className="navbar-toggler"
-					type="button"
-					data-bs-toggle="collapse"
-					data-bs-target="#navbarNavAltMarkup"
-					aria-controls="navbarNavAltMarkup"
-					aria-expanded="false"
-					aria-label="Toggle navigation"
-				>
-					<span className="navbar-toggler-icon" />
-				</button>
-				<div className="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
-					<div className="navbar-nav">
-						<a className="nav-link m-1" aria-current="page" href="#">
-							Perros
-						</a>
-						<a className="nav-link m-1" href="#">
-							Gatos
-						</a>
-						<Link to="/demo">
-							<button className="btn btn-light m-1">Iniciar Sesión</button>
-						</Link>
-						<Link to="/demo">
-							<button className="btn btn-light m-1">Registrase</button>
-						</Link>
-					</div>
-				</div>
-			</div>
-		</nav>
+  const [darkMode, setDarkMode] = useState(false);
+  const { store, actions } = useContext(Context);
 
-	);
+  useEffect(() => {
+    if (darkMode) {
+      document.body.classList.add("dark-mode");
+    } else {
+      document.body.classList.remove("dark-mode");
+    }
+  }, [darkMode]);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+
+  return (
+    <nav className={`navbar navbar-expand-lg ${darkMode ? "dark-mode" : ""}`} id="navbar">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="/">
+          <span className="navbar-brand mb-0 h1">No Tenemos Nombre Muchachos</span>
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div className="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+          <div className="navbar-nav">
+            <a className="nav-link m-1" aria-current="page" href="#">
+              Perros
+            </a>
+            <a className="nav-link m-1" href="#">
+              Gatos
+            </a>
+            <Link to="/demo">
+              <button className={`btn ${darkMode ? "btn-dark" : "btn-light"} m-1`}>Iniciar Sesión</button>
+            </Link>
+            <Link to="/demo">
+              <button className={`btn ${darkMode ? "btn-dark" : "btn-light"} m-1`}>Registrarse</button>
+            </Link>
+            <button className={`btn ${darkMode ? "btn-dark" : "btn-light"} m-1`} onClick={toggleDarkMode}>
+              {darkMode ? (
+                <i className="fa-regular fa-sun"></i>
+              ) : (
+                <i className="fa-regular fa-moon"></i>
+              )}
+              {darkMode ? "" : ""}
+            </button>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 };
-
-
