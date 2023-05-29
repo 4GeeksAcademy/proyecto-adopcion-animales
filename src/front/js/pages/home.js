e34ff1e05ece60f1f6026229a794f1dfbcf7a5f4
Juan Silva <123385530+juansj23@users.noreply.github.com>
2023-05-29 10:48:59 +0000

    info de adopcion añadido

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -3,13 +3,15 @@ import { Context } from "../store/appContext";
 
 import "../../styles/home.css";
 import { Carousel } from "../component/carousel";
+import InfoAdoption from "../component/infoAdoption";
 
 export const Home = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
-		<div className="text-center mt-5">
+		<div className="text-center">
 			<Carousel></Carousel>
+			<InfoAdoption></InfoAdoption>
 		</div>
 	);
 };
