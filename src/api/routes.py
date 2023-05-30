5ee362f76184b7b20def473e5f8c12f0f0611ff8
Jonathan Gutiérrez <120731494+Goodtierrez@users.noreply.github.com>
2023-05-30 16:22:58 +0000

    login with token ok

--- src/api/routes.py
+++ src/api/routes.py
@@ -10,6 +10,7 @@ from api.utils import generate_sitemap, APIException
 from flask_jwt_extended import create_access_token
 from flask_jwt_extended import get_jwt_identity
 from flask_jwt_extended import jwt_required
+from flask_jwt_extended import JWTManager
 
 api = Blueprint('api', __name__)
 
