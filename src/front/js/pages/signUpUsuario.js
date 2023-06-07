8189724e727da641408636b41db1926392b80d41
Juan Silva <123385530+juansj23@users.noreply.github.com>
2023-06-07 17:50:52 +0000

    fase2

--- src/front/js/pages/signUpUsuario.js
+++ src/front/js/pages/signUpUsuario.js
@@ -85,6 +85,7 @@ export default function SingUpUsuario() {
                     <p className="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">
                       Registro de Usuariosgi
                     </p>
+                    <h1>holaaa</h1>
                     <form className="mx-1 mx-md-4" onSubmit={handleSubmit}>
                       <div className="d-flex flex-row align-items-center mb-4">
                         <i className="fas fa-user fa-lg me-3 fa-fw" />
