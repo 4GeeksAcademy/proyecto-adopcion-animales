edd9f7c61167078bf93f37fd32164cc0dd7c8125
Juan Silva <123385530+juansj23@users.noreply.github.com>
2023-06-16 19:40:55 +0000

    nueva issue

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -84,10 +84,10 @@ export const Navbar = () => {
 					)}
 					<div className="navbar-nav">
 							<Link to="/donacion">
-								<button className="btn btn-light m-1">Haz una donacion</button>
+								<button className="btn  m-1" style={{backgroundColor:"#f9e4df"}}>Haz una donación</button>
 							</Link>
 						</div>
-					<button onClick={handleToggleDarkMode} className="btn btn-light m-1" style={{backgroundColor:"#A96D60"}}>
+					<button onClick={handleToggleDarkMode} className="btn  m-1" style={{backgroundColor:"#A96D60"}}>
 						{store.darkMode ? (
 							<>
 								<i className="fas fa-sun" alt="Modo claro" style={{ color:"white"}} ></i>
