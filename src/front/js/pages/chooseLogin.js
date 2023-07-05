393c9790e2efc585a6bd953058ed4408e45769f3
Juan Silva <123385530+juansj23@users.noreply.github.com>
2023-07-05 11:41:51 +0000

    casi listo los estilos

--- src/front/js/pages/chooseLogin.js
+++ src/front/js/pages/chooseLogin.js
@@ -18,32 +18,32 @@ export default function ChooseSignUp() {
     return (
         <div className='container my-1 justify-content-center vh-100'>
             <div className="container justify-content-center mb-5">
-                <img src={loginImg} className="img shadow-sm" id="loginImagen" style={{ maxWidth: "600px", maxHeight: "200px", display: "block", margin: "0 auto" }} />
+                <img src={loginImg} className="img" id="loginImagen" style={{ maxWidth: "600px", maxHeight: "200px", display: "block", margin: "0 auto" }} />
             </div>
             <div className="container">
                 <div className="row d-flex justify-content-center align-items-center">
                     <div className="col-sm-6">
-                        <div className="card border border-3 rounded-3 shadow-lg rounded-pill" style={{ backgroundColor: "transparent" }}>
+                        <div className="card border border-1 mt-2 shadow-sm" style={{ backgroundColor: "transparent" }}>
                             <div className='card-body text-center'>
                                 <h2 className="card-title">¿Soy un Usuario?</h2>
                                 <p className="card-text">
                                     ¡Registrado y listo para adoptar a mi nueva mascota!
                                 </p>
                                 <Link to="/loginUsuario">
-                                    <button type="button" className="btn btn-lg shadow-sm" style={{ backgroundColor: "#ff914d", textDecoration: "none", color: "black" }}>Iniciar Sesión</button>
+                                    <button type="button" className="btn btn-lg shadow-sm mt-2" style={{ backgroundColor: "#ff914d", textDecoration: "none", color: "black" }}>Iniciar Sesión</button>
                                 </Link>
                             </div>
                         </div>
                     </div>
                     <div className="col-sm-6">
-                        <div className="card border border-3 rounded-3 shadow-lg rounded-pill" style={{ backgroundColor: "transparent" }}>
+                        <div className="card border border-1 mt-2 shadow-sm" style={{ backgroundColor: "transparent" }}>
                             <div className='card-body text-center'>
                                 <h2 className="card-title">¿Soy una Asociación?</h2>
                                 <p className="card-text">
                                     ¡Registrada como asociación y lista para ayudar a los animales!
                                 </p>
                                 <Link to="/loginAsociacion">
-                                    <button type="button" className="btn btn-lg shadow-sm" style={{ backgroundColor: "#ff914d", textDecoration: "none", color: "black" }}>Iniciar Sesión</button>
+                                    <button type="button" className="btn btn-lg shadow-sm mt-2" style={{ backgroundColor: "#ff914d", textDecoration: "none", color: "black" }}>Iniciar Sesión</button>
                                 </Link>
                             </div>
                         </div>
--- src/front/js/pages/donacion.js
+++ src/front/js/pages/donacion.js
@@ -8,72 +8,74 @@ export default function Donacion() {
     return (
         <div className='vh-100 text-center justify-content-center'>
             <div className='container mt-4'>
-                <img src={donacionImg} className="img shadow-sm" id="loginImagen" style={{ maxWidth: "600px", maxHeight: "200px", display: "block", margin: "0 auto" }} />
-                <div className='container mt-4  border rounded-pill shadow border-3 p-2'>
+                <img src={donacionImg} className="img" id="loginImagen" style={{ maxWidth: "600px", maxHeight: "200px", display: "block", margin: "0 auto" }} />
+                <div className='container mt-4 p-2'>
                     <p style={{ margin: "0 auto" }}>
                         Con solo 9.99€, puedes ayudar a más de un animal necesitado. Tu apoyo es crucial para proporcionarles alimento y un hogar seguro. Únete a nosotros en esta noble causa y juntos crearemos un impacto positivo en la vida de estos indefensos seres. ¡Tu donación cuenta!
                     </p>
-                </div>
-            </div>
-            <div className='container mt-5'>
-                <div className='row justify-content-center'>
-                    <div className='col-md-12'>
-                        <div className='d-flex justify-content-center'>
-                            <PayPalScriptProvider
-                                options={{
-                                    'client-id': 'AXAbpXS5XM3nNJCq8gVcVRv-x2cFxB9SPAuHdJ6nPTfQuS6CvS9VJ-tO6odW35x6Fo5XfWIqvEbAALR9',
-                                    currency: "EUR"
-                                    
-                                }}
-                            >
-                                <PayPalButtons
-                                fundingSource="paypal"
-                                style={{"layout":"vertical","label":"donate"}}
-                                disabled={false}
-                                createOrder={(data, actions) => {
-                                    return actions.order
-                                        .create({
-                                            purchase_units: [
-                                                {
-                                                    amount: {
-                                                        value: "9.99",
-                                                        breakdown: {
-                                                            item_total: {
-                                                                currency_code: "EUR",
-                                                                value: "9.99",
+                    <div className='container mt-5'>
+                        <div className='row justify-content-center'>
+                            <div className='col-md-12'>
+                                <div className='d-flex justify-content-center'>
+                                    <PayPalScriptProvider
+                                        options={{
+                                            'client-id': 'AXAbpXS5XM3nNJCq8gVcVRv-x2cFxB9SPAuHdJ6nPTfQuS6CvS9VJ-tO6odW35x6Fo5XfWIqvEbAALR9',
+                                            currency: "EUR"
+
+                                        }}
+                                    >
+                                        <PayPalButtons
+                                            fundingSource="paypal"
+                                            style={{ "layout": "vertical", "label": "donate" }}
+                                            disabled={false}
+                                            createOrder={(data, actions) => {
+                                                return actions.order
+                                                    .create({
+                                                        purchase_units: [
+                                                            {
+                                                                amount: {
+                                                                    value: "9.99",
+                                                                    breakdown: {
+                                                                        item_total: {
+                                                                            currency_code: "EUR",
+                                                                            value: "9.99",
+                                                                        },
+                                                                    },
+                                                                },
+                                                                items: [
+                                                                    {
+                                                                        name: "donation-example",
+                                                                        quantity: "1",
+                                                                        unit_amount: {
+                                                                            currency_code: "EUR",
+                                                                            value: "9.99",
+                                                                        },
+                                                                        category: "DONATION",
+                                                                    },
+                                                                ],
                                                             },
-                                                        },
-                                                    },
-                                                    items: [
-                                                        {
-                                                            name: "donation-example",
-                                                            quantity: "1",
-                                                            unit_amount: {
-                                                                currency_code: "EUR",
-                                                                value: "9.99",
-                                                            },
-                                                            category: "DONATION",
-                                                        },
-                                                    ],
-                                                },
-                                            ],
-                                        })
-                                        .then((orderId) => {
-                                            // Your code here after create the donation
-                                            return orderId;
-                                        });
-                                }}
-                                    onApprove={(data, actions) => {
-                                        return actions.order.capture().then(function (details) {
-                                            alert('Transaction completed by ' + details.payer.name.given_name);
-                                        });
-                                    }}
-                                />
-                            </PayPalScriptProvider>
+                                                        ],
+                                                    })
+                                                    .then((orderId) => {
+                                                        // Your code here after create the donation
+                                                        return orderId;
+                                                    });
+                                            }}
+                                            onApprove={(data, actions) => {
+                                                return actions.order.capture().then(function (details) {
+                                                    alert('Transaction completed by ' + details.payer.name.given_name);
+                                                });
+                                            }}
+                                        />
+                                    </PayPalScriptProvider>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
+
             </div>
+
         </div>
 
     )
